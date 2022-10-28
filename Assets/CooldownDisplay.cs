2bcaa2e94d230f4488f653859a2ba6773d1da524
Fappyzz <92708533+Fappyzz@users.noreply.github.com>
2022-10-28 16:54:09 +0800

    Cooldown timer visuals works
    But need to connect to the units.

--- Assets/CooldownDisplay.cs
+++ Assets/CooldownDisplay.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using static GameData;
+using static Unitf;
 
 public class CooldownDisplay : MonoBehaviour
 {
     [SerializeField] Image clockCD;
     [SerializeField] TextMeshProUGUI cooldownTimer;
+
+    bool skillpressed;
+
+    int cooldown;
+    float countdownCooldown;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,13 +25,35 @@ public class CooldownDisplay : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        cooldownTimer.text = Mathf.FloorToInt(countdownCooldown).ToString(); 
+        if (skillpressed)
+        {
+            Countdown();
+            countdownCooldown -= 1 * Time.deltaTime;
+            
+        }
+    }
+
+    public void SkillPressed()
+    {
+        clockCD.fillAmount = 1;
+        clockCD.gameObject.SetActive(true);
+        skillpressed = true;
+        cooldown = 10;
+        countdownCooldown = 10;
+    }
+    public void Countdown()
+    {
+        clockCD.fillAmount -= Time.deltaTime/cooldown ;
+        if (clockCD.fillAmount <= 0)
+        {
+            skillpressed = false;
+            clockCD.gameObject.SetActive(false);
+
+        }
         
     }
 
-    public void skillPressed()
-    {
-
-    }
-
+    
 
 }
