2acf49411a9da5d8897acaa83a90d965cf442715
Fappyzz <92708533+Fappyzz@users.noreply.github.com>
2022-10-31 08:22:56 +0800

    edit heat bar

--- Assets/Scripts/Heatbar.cs
+++ Assets/Scripts/Heatbar.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using static GameData;
 using TMPro;
 
@@ -8,16 +9,24 @@ public class Heatbar : MonoBehaviour
 {
     public TextMeshProUGUI mHeat;
     public TextMeshProUGUI cHeat;
+
+    [SerializeField] Image fill;
+    [SerializeField]float Fillfloat;
+
     // Start is called before the first frame update
     void Start()
     {
         
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        Fillfloat = Heat * 0.02f;
+        fill.fillAmount = Fillfloat;
         mHeat.text = MaxHeat.ToString();
         cHeat.text = Heat.ToString();
+        
     }
 }
